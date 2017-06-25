a9bf8d467dd00c4949992d008ea5cb5e66ff2da5
mladen994 <mladen994@gmail.com>
2017-06-25 20:43:57 +0200

    feat (*): temp commit

---
+++ app/src/main/java/rs/elfak/miksa_mladen/peaktracktion/adapters/PlacesAdapter.java
@@ -0,0 +1,8 @@
+package rs.elfak.miksa_mladen.peaktracktion.adapters;
+
+/**
+ * Created by miksa on 25.6.17..
+ */
+
+public class PlacesAdapter {
+}
---
+++ app/src/main/java/rs/elfak/miksa_mladen/peaktracktion/list_items/Place.java
@@ -0,0 +1,8 @@
+package rs.elfak.miksa_mladen.peaktracktion.list_items;
+
+/**
+ * Created by miksa on 25.6.17..
+ */
+
+public class Place {
+}
