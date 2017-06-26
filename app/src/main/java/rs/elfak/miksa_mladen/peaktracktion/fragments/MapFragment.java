addf14af7888e08d46bd14e2934bd9f5defc86ea
Pritilender <sajn_ap@live.com>
2017-06-27 01:42:17 +0200

    fix build fail

--- app/src/main/java/rs/elfak/miksa_mladen/peaktracktion/fragments/MapFragment.java
+++ app/src/main/java/rs/elfak/miksa_mladen/peaktracktion/fragments/MapFragment.java
@@ -13,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
